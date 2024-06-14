b4e422c446dbb3197d129cfd71a2d3aaa793b072
areksoulahian <arek.soulahian@gmail.com>
2024-06-14 15:14:48 -0700

    Add files via upload

--- backend/routes/foods.js
+++ backend/routes/foods.js
@@ -1,53 +1,70 @@
 const router = require("express").Router();
-let Food = require("../models/food.model");
 
-router.route("/").get((req, res) => {
-  Food.find()
-    .then((foods) => res.json(foods))
-    .catch((err) => res.status(400).json("Error: " + err));
+const {
+  getFoods,
+  createFood,
+  getFoodbyId,
+  updateFood,
+  deleteFood,
+} = require("../models/food.model");
+
+router.route("/").get(async (req, res) => {
+  try {
+    const foods = await getFoods();
+    res.json(foods);
+  } catch (error) {
+    res.status(400).json("Get Food Error: " + error.message);
+  }
 });
 
-router.route("/add").post((req, res) => {
-  const username = req.body.username;
-  const foodName = req.body.foodName;
-  const calories = req.body.calories;
-  /* tslint:disable:no-unused-variable */
-  //   const date = req.body.date;
+router.route("/add").post(async (req, res) => {
+  const { username, foodName, calories, date } = req.body;
 
-  const newFood = new Food({ username, foodName, calories });
-
-  newFood
-    .save()
-    .then(() => res.json("Food added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+  try {
+    const newFood = await createFood({ username, foodName, calories, date });
+    res.json("Food added: " + newFood);
+  } catch (error) {
+    res.status(400).json("New Food Error: " + error.message);
+  }
 });
 
-router.route("/:id").get((req, res) => {
-  Food.findById(req.params.id)
-    .then((food) => res.json(food))
-    .catch((err) => res.status(400).json("Error: " + err));
+router.route("/:id").get(async (req, res) => {
+  try {
+    const food = await getFoodbyId(req.params.id);
+    if (food) {
+      res.json(food);
+    } else {
+      res.status(404).json("Food not found");
+    }
+  } catch (error) {
+    res.status(400).json("Error: " + error.message);
+  }
 });
 
-router.route("/:id").delete((req, res) => {
-  Food.findByIdAndDelete(req.params.id)
-    .then(() => res.json("Food deleted."))
-    .catch((err) => res.status(400).json("Error: " + err));
+router.route("/:id").delete(async (req, res) => {
+  try {
+    const success = await deleteFood(req.params.id);
+    if (success) {
+      res.json("Food deleted");
+    } else {
+      res.status(404).json("food not found to delete");
+    }
+  } catch (error) {
+    res.status(400).json("Error: " + error.message);
+  }
 });
 
-router.route("/update/:id").post((req, res) => {
-  Food.findById(req.params.id)
-    .then((food) => {
-      food.username = req.body.username;
-      food.foodName = req.body.foodName;
-      food.calories = req.body.calories;
-      food.date = Date.parse(req.body.date);
-
-      food
-        .save()
-        .then(() => res.json("Food updated!"))
-        .catch((err) => res.status(400).json("Error: " + err));
-    })
-    .catch((err) => res.status(400).json("Error: " + err));
+router.route("/update/:id").post(async (req, res) => {
+  try {
+    const updatedFood = await updateFood(req.params.id, req.body);
+    if (updatedFood) {
+      res.json("Food updated");
+    } else {
+      res.status(404).json("food not found to update");
+    }
+  } catch (error) {
+    res.status(400).json("Error: " + error.message);
+  }
 });
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,21 +1,25 @@
 const router = require("express").Router();
-let User = require("../models/user.model");
 
-router.route("/").get((req, res) => {
-  User.find()
-    .then((users) => res.json(users))
-    .catch((err) => res.status(400).json("Error: " + err));
+const { getUsers, createUser } = require("../models/user.model");
+
+router.route("/").get(async (req, res) => {
+  try {
+    const users = await getUsers();
+    res.json(users);
+  } catch (error) {
+    res.status(400).json("Get User Error: " + error.message);
+  }
 });
 
-router.route("/add").post((req, res) => {
-  const username = req.body.username;
+router.route("/add").post(async (req, res) => {
+  const { username } = req.body;
 
-  const newUser = new User({ username });
-
-  newUser
-    .save()
-    .then(() => res.json("User added!"))
-    .catch((err) => res.status(400).json("Error: " + err));
+  try {
+    const newUser = await createUser({ username });
+    res.json("User added: " + newUser);
+  } catch (error) {
+    res.status(400).json("New User Error: " + error.message);
+  }
 });
 
 module.exports = router;
